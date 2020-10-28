705352c12c24558a8c993eb19a0ab0f800422a79
warri <41282717+Emuohwo@users.noreply.github.com>
2020-10-28 20:44:49 +0100

    Remove deleted module css from _app

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme';
-import 'pure-react-carousel/dist/react-carousel.es.css';
 import "react-slideshow-image/dist/styles.css";
 
 export default function MyApp(props) {
